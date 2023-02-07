78e3328e5876e62bcbe144cae07059f5c8e225c1
Amirbek <124518356+Merkavacorporation@users.noreply.github.com>
2023-02-07 19:24:32 +0500

    Add files via upload

---
+++ Merkabatec.js
@@ -0,0 +1,19 @@
+var merk=1
+var cost=3000
+var pul=4000
+var gad=5000
+var prog=merk * cost
+
+   if(pul>prog)
+{console.log('Pulingiz yetadi')}
+
+   else if(pul<prog)
+{console.log('Pulingiz yetmaydi')}
+
+
+   else if((pul>=prog)<=gad)
+{console.log('Gadoy puling yetadi')}
+
+
+
+   
